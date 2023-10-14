0053cb9564bf874e817dd335964464866cfcffe6
ziommworld <147875598+ziommworld@users.noreply.github.com>
2023-10-14 13:13:37 +0300

    major update get_selector

--- widgets.py
+++ widgets.py
@@ -1,14 +1,13 @@
 import ipywidgets as widgets
-from IPython.display import display
 
-
-def get_selector(dataframe, column, selection):
+def get_selector(id, dataframe, column, selection):
     """Test"""
+    options = dataframe[column]
 
     dropdown = widgets.Dropdown(
-        options=dataframe[column],
+        options=options,
         value=None,
-        description='Choose:',
+        description=id,
     )
 
     slider = widgets.IntSlider(
@@ -17,25 +16,55 @@ def get_selector(dataframe, column, selection):
         max=10,
         step=1,
         description='Number:',
+        disabled=True,
         continuous_update=False  # update the value only when the user releases the slider handle
+    )
+    
+    label = widgets.Label(value="")
+
+    button = widgets.Button(
+        description='Reset'
     )
 
     def update_dropdown(change):
-        dropdown.value = change['new']
+      if change['name'] == 'label':
         slider.value = 1
 
-        selection[dropdown.value] = slider.value
+        new_value = change['new']
+        if new_value is not None:
+          slider.disabled = False
+          slider.max = dataframe.loc[dataframe[column] == new_value, 'Stack'].values[0]
+          label.value = f"max stack: {slider.max}"
+          selection[new_value] = slider.value
+        else:
+          slider.disabled = True
+        
+        old_value = change['old']
+        if old_value is not None:
+          del selection[old_value]
 
+        %clear
+        print(selection)
+        
     def update_slider(change):
+      if change['name'] == 'value':
         slider.value = change['new']
 
-        selection[dropdown.value] = slider.value
+        if (dropdown.value):
+            selection[dropdown.value] = slider.value
+        
+        %clear
+        print(selection)
+
+    def reset_dropdown(button):
+      dropdown.value = None
 
     dropdown.observe(update_dropdown)
     slider.observe(update_slider)
+    button.on_click(reset_dropdown)
 
     # Display the dropdown and output widgets
-    box = widgets.HBox([dropdown, slider])
+    box = widgets.HBox([dropdown, slider, button, label])
 
     display(box)
 
